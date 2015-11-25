86130a9b8bf124f0fb1519d51acea4d99b16febf
yanyan2060 <yanyan9112@gmail.com>
2015-11-25 12:24:08 -0800

    Create 107. levelorder 2.py

---
+++ 107. levelorder 2.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root == None: return []
+        curlevel = [root]
+        res = []
+        while curlevel:
+            nextlevel = []
+            number = []
+            for node in curlevel:
+                number.append(node.val)
+                if node.left:
+                    nextlevel.append(node.left)
+                if node.right:
+                    nextlevel.append(node.right)
+            res.insert(0,number)
+            curlevel = nextlevel
+        return res
+        
+        
